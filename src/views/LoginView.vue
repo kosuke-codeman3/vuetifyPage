eb0a210d33470997a71fb505b7b9a5306e7ee719
kosuke-codeman3 <siratamatabetai@gmail.com>
2022-12-26 18:03:23 +0900

    feat:環境作成

---
+++ src/views/LoginView.vue
@@ -0,0 +1,16 @@
+<template>
+  <Login />
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Login from "../components/pages/login/index.vue";
+
+export default Vue.extend({
+  //name: "Home",
+
+  components: {
+    Login,
+  },
+});
+</script>
