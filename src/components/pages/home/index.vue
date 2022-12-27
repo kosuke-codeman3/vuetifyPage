9215c77bf4a75538e6fb46a80cedffdc8e25a2d1
kosuke-codeman3 <siratamatabetai@gmail.com>
2022-12-27 11:20:47 +0900

    feat:headerの作成

--- src/components/pages/home/index.vue
+++ src/components/pages/home/index.vue
@@ -1,7 +1,15 @@
-<!--ソースもと-->
-<!--https://reffect.co.jp/vue/vuetify-first-time-->
 <template>
-  <v-btn class="info" @click="submit">ログイン</v-btn>
+  <v-app>
+    <div>
+      <v-app-bar color="primary">
+        <v-toolbar-title class="white--text">LOGO</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-btn class="mx-2" fab light small>
+          <v-icon light class="text-right">mdi-account</v-icon>
+        </v-btn>
+      </v-app-bar>
+    </div>
+  </v-app>
 </template>
 
 <script>
